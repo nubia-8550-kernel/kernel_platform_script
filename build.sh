a314c850d7a8edeef982241de23dbb1f802c54c8
chandu078 <chandudyavanapelli03@gmail.com>
2023-06-12 03:55:53 +0000

    script: Add required modules for kalama
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- build.sh
+++ build.sh
@@ -22,10 +22,18 @@ export EXT_MODULES="
   ../vendor/qcom/opensource/datarmnet-ext/perf_tether
   ../vendor/qcom/opensource/datarmnet-ext/shs
   ../vendor/qcom/opensource/datarmnet-ext/wlan
+  ../vendor/qcom/opensource/mm-drivers/hw_fence
+  ../vendor/qcom/opensource/mm-drivers/msm_ext_display
+  ../vendor/qcom/opensource/mm-drivers/sync_fence
+  ../vendor/qcom/opensource/securemsm-kernel
   ../vendor/qcom/opensource/display-drivers/msm
   ../vendor/qcom/opensource/eva-kernel
   ../vendor/qcom/opensource/video-driver
+  ../vendor/qcom/opensource/graphics-kernel
+  ../vendor/qcom/opensource/touch-drivers
+  ../vendor/qcom/opensource/wlan/platform
   ../vendor/qcom/opensource/wlan/qcacld-3.0/.qca6490
+  ../vendor/qcom/opensource/bt-kernel
 "
 
 export LTO=thin
